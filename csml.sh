b94edd98fcd69ae51f599094ce36500f946fc1a8
felifrog <ogibenefra@gmail.com>
2018-02-25 16:44:13 +0100

    Added encryption binaries

--- csml.sh
+++ csml.sh
@@ -1,6 +1,8 @@
-# echo "INSERT INTO md5(decryption,ecryption) VALUES" > $3
+echo "Welcome in csml-v1.0! [Press any key to continue]";
+read var;
+echo "INSERT INTO md5(decryption,ecryption) VALUES" > $3
 while read p; do
   if [ ! -z "$p" ]; then
     echo "(\"$p\", \"$($1 $p)\")," >> $3
   fi
-done < <(crunch 1 $2)
+done < $2
